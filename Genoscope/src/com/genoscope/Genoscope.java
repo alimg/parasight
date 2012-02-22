ee85f4e2e66da9d9c515682ad2e3e2c957c37f50
Alim Gokkaya <alim.cpp@gmail.com>
2012-02-23 01:34:46 +0200

    gui initials

--- Genoscope/src/com/genoscope/Genoscope.java
+++ Genoscope/src/com/genoscope/Genoscope.java
@@ -4,20 +4,96 @@
  */
 package com.genoscope;
 
+import java.awt.*;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLProfile;
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.awt.GLJPanel;
+import javax.media.opengl.awt.GLCanvas;
+import javax.swing.JFrame;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JPanel;
 /**
  *
  * @author alim
  */
 public class Genoscope {
 
+    static {
+        // setting this true causes window events not to get sent on Linux if you run from inside Eclipse
+        GLProfile.initSingleton( false );
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        GenoscopeGUIMain.main(args);
+        GLProfile glprofile = GLProfile.getDefault();
+        GLCapabilities glcapabilities = new GLCapabilities( glprofile );
+        final GLCanvas glcanvas = new GLCanvas( glcapabilities );
+
+        glcanvas.addGLEventListener( new GLEventListener() {
+            
+            @Override
+            public void reshape( GLAutoDrawable glautodrawable, int x, int y, int width, int height ) {
+                GLRenderer.setup( glautodrawable.getGL().getGL2(), width, height );
+            }
+            
+            @Override
+            public void init( GLAutoDrawable glautodrawable ) {
+            }
+            
+            @Override
+            public void dispose( GLAutoDrawable glautodrawable ) {
+            }
+            
+            @Override
+            public void display( GLAutoDrawable glautodrawable ) {
+                GLRenderer.render( glautodrawable.getGL().getGL2(), glautodrawable.getWidth(), glautodrawable.getHeight() );
+            }
+        });/*
+    	//MyPanel canvas=new MyPanel();
+        JFrame frame = new JFrame( "Hello World" );
+        //frame.getContentPane().add( canvas);
+        JPanel p=new com.genoscope.GLJPanel();
+        p.setLayout(new FlowLayout());
+        p.add(glcanvas);
+        p.add(new TextField("deneme"));
+        p.revalidate();
+        frame.getContentPane().add( p);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent ev) {
+                System.exit(0);
+            }
+        });
+
+
+        frame.setSize( frame.getContentPane().getPreferredSize() );
+        frame.setVisible( true );
+        //frame.getContentPane().add( glcanvas, BorderLayout.CENTER );
         
+        frame.pack();
+        frame.setSize( 640, 480 );
+        frame.setVisible( true );
+        * 
+        */
+        
+        GenoscopeApp f=new GenoscopeApp();
+        f.getContentPane().add(new TextField("deneme"));
+        f.setVisible(true);
+        
+        f.jPanel1.removeAll();
+        //f.jPanel1.setPreferredSize(new Dimension(0,0));
+        f.jPanel1.layout();
+        
+        f.jPanel1.add(glcanvas);
+        //f.jPanel1.add(new TextField("deneme"));
+        //f.main(args);
+
         System.out.println("ends\n");
         
     }
--- Genoscope/src/com/genoscope/GenoscopeApp.java
+++ Genoscope/src/com/genoscope/GenoscopeApp.java
@@ -8,12 +8,12 @@ package com.genoscope;
  *
  * @author alim
  */
-public class GenoscopeGUIMain extends javax.swing.JFrame {
+public class GenoscopeApp extends javax.swing.JFrame {
 
     /**
-     * Creates new form GenosightGUIMain
+     * Creates new form GenoscopeApp
      */
-    public GenoscopeGUIMain() {
+    public GenoscopeApp() {
         initComponents();
     }
 
@@ -26,6 +26,7 @@ public class GenoscopeGUIMain extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         menuBar = new javax.swing.JMenuBar();
         fileMenu = new javax.swing.JMenu();
         openMenuItem = new javax.swing.JMenuItem();
@@ -42,7 +43,8 @@ public class GenoscopeGUIMain extends javax.swing.JFrame {
         aboutMenuItem = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Genoscope");
+
+        jPanel1.setLayout(new java.awt.CardLayout());
 
         fileMenu.setMnemonic('f');
         fileMenu.setText("File");
@@ -111,11 +113,17 @@ public class GenoscopeGUIMain extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(65, 65, 65)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE)
+                .addGap(66, 66, 66))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 279, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 229, Short.MAX_VALUE)
+                .addGap(30, 30, 30))
         );
 
         pack();
@@ -146,13 +154,13 @@ public class GenoscopeGUIMain extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GenoscopeGUIMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GenoscopeApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GenoscopeGUIMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GenoscopeApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GenoscopeGUIMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GenoscopeApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GenoscopeGUIMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GenoscopeApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -162,7 +170,7 @@ public class GenoscopeGUIMain extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
 
             public void run() {
-                new GenoscopeGUIMain().setVisible(true);
+                new GenoscopeApp().setVisible(true);
             }
         });
     }
@@ -176,6 +184,7 @@ public class GenoscopeGUIMain extends javax.swing.JFrame {
     private javax.swing.JMenuItem exitMenuItem;
     private javax.swing.JMenu fileMenu;
     private javax.swing.JMenu helpMenu;
+    public javax.swing.JPanel jPanel1;
     private javax.swing.JMenuBar menuBar;
     private javax.swing.JMenuItem openMenuItem;
     private javax.swing.JMenuItem pasteMenuItem;
