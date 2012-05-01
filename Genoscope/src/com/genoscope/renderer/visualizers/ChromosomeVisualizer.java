a34b193b243e15332a534259c9a3072074508c58
Ahmet Kerim ŞENOL <Ahmet Kerim ENOL@192.168.2.214>
2012-05-01 15:23:58 +0300

    Hizlandi

--- Genoscope/src/com/genoscope/renderer/visualizers/ChromosomeVisualizer.java
+++ Genoscope/src/com/genoscope/renderer/visualizers/ChromosomeVisualizer.java
@@ -20,7 +20,7 @@ public class ChromosomeVisualizer extends Visualizer{
         this.chromosome = chr;
     }
     public float getPosX(int position){
-        System.out.println("length = " + chromosome.getLength());
+      
         return (float)getWidth()*((float)(position-chromosome.getStart())/chromosome.getLength());
     }
     @Override
