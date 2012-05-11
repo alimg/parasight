db3862e0e7f786b87b588982d1d4c63c0452a990
Ahmet Kerim ŞENOL <Ahmet Kerim ENOL@192.168.2.214>
2012-05-11 18:55:29 +0300

    Renk

---
+++ Genoscope/src/com/genoscope/ColorPicker.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.genoscope;
+import com.genoscope.types.Color;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ahmet Kerim ŞENOL
+ */
+
+public class ColorPicker {
+    private static ArrayList<Color> colors = new ArrayList<Color>();
+   
+    public static void addColor(Color c){
+        colors.add(c);
+    }
+    
+    public static Color getColor(String name){
+        Color ret=null;
+        for(Color c: colors){
+            if(c.getName().equals(name.toLowerCase()))
+                ret=c;
+        }
+        return ret;
+    }
+    
+}
