232f4fdfd2985c7779d88d982afcd6e3e639f351
Furkan Mustafa Akdemir <Furkan Mustafa Akdemir@Fma-pc.mshome.net>
2012-05-12 06:40:16 +1200

    input change

--- Genoscope/src/com/genoscope/reader/ColorReader.java
+++ Genoscope/src/com/genoscope/reader/ColorReader.java
@@ -4,10 +4,23 @@
  */
 package com.genoscope.reader;
 
+import java.io.File;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Furkan Mustafa Akdemir
  */
 public class ColorReader {
-
+	public static void readFile(String path){
+		File f=new File(".");
+		System.out.println(f.getAbsolutePath());
+		final JPanel panel = new JPanel();
+			JOptionPane.showMessageDialog(panel, "'"+f.getAbsolutePath()+"'",
+					"Warning", JOptionPane.WARNING_MESSAGE);
+/*		Scanner scan = new Scanner(new File("resources/color.cl"));
+		scan.nextLine();
+*/	}
 }
